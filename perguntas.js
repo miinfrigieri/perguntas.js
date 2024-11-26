e102eb552bca6a023810a5a58ac83a411dc15c43
miinfrigieri <0000110230220xsp@al.educacao.sp.gov.br>
2024-11-25 22:44:37 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+'Programação',
+'O que é Python?',
+'O Python é uma linguagem de programação'
+)
+criaCartao(
+'Programação',
+'O que é Python?',
+'O Python é uma linguagem de programação'
+)
+criaCartao(
+'Programação',
+'O que é Python?',
+'O Python é uma linguagem de programação'
+)
+criaCartao(
+'Lingua inglesa',
+'Como se diz oi em Inglês?',
+'Oi em ingles é HI (RAI)'
+)
